dd19d3d7a440d4e5645d402e601bd9bc01416a37
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-08-04 02:28:49 +0300

    added tests

--- main_test.go
+++ main_test.go
@@ -32,3 +32,59 @@ func TestNumberOfLines(t *testing.T) {
 		t.Errorf("Expected %s but got %s", expectedOutput, string(output))
 	}
 }
+
+func TestNumberOfWords(t *testing.T) {
+	cmd := exec.Command("go", "run", "main.go", "-w", "test.txt")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedOutput := "58164 test.txt\n"
+
+	if string(output) != expectedOutput {
+		t.Errorf("Expected %s but got %s", expectedOutput, string(output))
+	}
+}
+
+func TestNumberOfCharacters(t *testing.T) {
+	cmd := exec.Command("go", "run", "main.go", "-m", "test.txt")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedOutput := "339292 test.txt\n"
+
+	if string(output) != expectedOutput {
+		t.Errorf("Expected %s but got %s", expectedOutput, string(output))
+	}
+}
+
+func TestDefault(t *testing.T) {
+	cmd := exec.Command("go", "run", "main.go", "test.txt")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedOutput := "7145  58164 342190 test.txt\n"
+
+	if string(output) != expectedOutput {
+		t.Errorf("Expected %s but got %s", expectedOutput, string(output))
+	}
+}
+
+func TestStdin(t *testing.T) {
+	cmd := exec.Command("cat", "test.txt", "|", "go", "run", "main.go", "-l")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedOutput := "7145\n"
+
+	if string(output) != expectedOutput {
+		t.Errorf("Expected %s but got %s", expectedOutput, string(output))
+	}
+}
