f6f38a19ff6fed7ef10c29d6aa4eee8f5b3cb65c
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-08-04 02:49:28 +0300

    added impl for number of words

--- main.go
+++ main.go
@@ -33,16 +33,40 @@ func getLinesCount(filename string) int {
 	return lines
 }
 
+func getWordsCount(filename string) int {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		file.Close()
+		os.Exit(1)
+	}
+	defer file.Close()
+	words := 0
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		words++
+	}
+	return words
+}
+
 func main() {
 	args := os.Args[1:]
-	flag := args[0]
-	filename := args[1]
+	flag := ""
+	filename := args[0]
+	if len(args) == 2 {
+		flag = args[0]
+		filename = args[1]
+	}
+
 	switch flag {
 	case "-c":
 		fmt.Println(getBytesCount(filename), filename)
 	case "-l":
 		fmt.Println(getLinesCount(filename), filename)
+	case "-w":
+		fmt.Println(getWordsCount(filename), filename)
 	default:
-		fmt.Println(getLinesCount(filename), getBytesCount(filename), filename)
+		fmt.Println(getLinesCount(filename), getWordsCount(filename), getBytesCount(filename), filename)
 	}
 }
