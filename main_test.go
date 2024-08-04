eded4ae7cf3c0cf6556808c342e770deeeb8711d
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-08-04 07:13:03 +0300

    test support multiple files

--- main_test.go
+++ main_test.go
@@ -3,52 +3,82 @@ package main
 import (
 	"bytes"
 	"os/exec"
+	"strings"
 	"testing"
 )
 
-func runFileTest(t *testing.T, cmd *exec.Cmd, expectedOutput string) {
+var files = []string{"test_data/test1.txt", "test_data/test2.txt", "test_data/test3.txt"}
+
+func runFileTest(t *testing.T, args []string, expectedOutputs []string) {
+	cmdArgs := append([]string{"run", "main.go"}, args...)
+	cmd := exec.Command("go", cmdArgs...)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if string(output) != expectedOutput {
-		t.Errorf("Expected %s but got %s", expectedOutput, string(output))
+	outputStr := string(output)
+	for _, expectedOutput := range expectedOutputs {
+		if !strings.Contains(outputStr, expectedOutput) {
+			t.Errorf("Expected %s but got %s", expectedOutput, outputStr)
+		}
 	}
 }
 
 func TestNumberOfBytes(t *testing.T) {
-	cmd := exec.Command("go", "run", "main.go", "-c", "test_data/test.txt")
-	expectedOutput := "342190 test_data/test.txt\n"
-	runFileTest(t, cmd, expectedOutput)
+	args := append([]string{"-c"}, files...)
+	expectedOutputs := []string{
+		"5 test_data/test1.txt\n",
+		"12 test_data/test2.txt\n",
+		"335045 test_data/test3.txt\n",
+		"335062 total\n",
+	}
+	runFileTest(t, args, expectedOutputs)
 }
 
 func TestNumberOfLines(t *testing.T) {
-	cmd := exec.Command("go", "run", "main.go", "-l", "test_data/test.txt")
-	expectedOutput := "7145 test_data/test.txt\n"
-	runFileTest(t, cmd, expectedOutput)
+	args := append([]string{"-l"}, files...)
+	expectedOutputs := []string{
+		"1 test_data/test1.txt\n",
+		"2 test_data/test2.txt\n",
+		"7145 test_data/test3.txt\n",
+		"7148 total\n",
+	}
+	runFileTest(t, args, expectedOutputs)
 }
 
 func TestNumberOfWords(t *testing.T) {
-	cmd := exec.Command("go", "run", "main.go", "-w", "test_data/test.txt")
-	expectedOutput := "58164 test_data/test.txt\n"
-	runFileTest(t, cmd, expectedOutput)
+	args := append([]string{"-w"}, files...)
+	expectedOutputs := []string{
+		"1 test_data/test1.txt\n",
+		"2 test_data/test2.txt\n",
+		"58164 test_data/test3.txt\n",
+		"58167 total\n",
+	}
+	runFileTest(t, args, expectedOutputs)
 }
 
 func TestNumberOfCharacters(t *testing.T) {
-	cmd := exec.Command("go", "run", "main.go", "-m", "test_data/test.txt")
-	expectedOutput := "339292 test_data/test.txt\n"
-	runFileTest(t, cmd, expectedOutput)
+	args := []string{"-m", "test_data/test3.txt"}
+	expectedOutputs := []string{
+		"332147 test_data/test3.txt\n",
+	}
+	runFileTest(t, args, expectedOutputs)
 }
 
 func TestDefault(t *testing.T) {
-	cmd := exec.Command("go", "run", "main.go", "test_data/test.txt")
-	expectedOutput := "7145 58164 342190 test_data/test.txt\n"
-	runFileTest(t, cmd, expectedOutput)
+	args := append([]string{}, files...)
+	expectedOutputs := []string{
+		"1 1 5 test_data/test1.txt\n",
+		"2 2 12 test_data/test2.txt\n",
+		"7145 58164 335045 test_data/test3.txt\n",
+		"7148 58167 335062 total\n",
+	}
+	runFileTest(t, args, expectedOutputs)
 }
 
 func TestLinesFromStdin(t *testing.T) {
-	catCmd := exec.Command("cat", "test_data/test.txt")
+	catCmd := exec.Command("cat", "test_data/test3.txt")
 	goCmd := exec.Command("go", "run", "main.go", "-l")
 
 	var catOut bytes.Buffer
